b2bfad6f9e94f42eb68dc33c9c262a5903f5a1fe
zafarirum87 <75549633+zafarirum87@users.noreply.github.com>
2023-03-21 15:57:34 +0200

    Initial commit

---
+++ src/main/java/com/example/HealthMonitoringApplication/HealthMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.HealthMonitoringApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HealthMonitoringApplication/HealthMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HealthMonitoringApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
